2a24c9d4a0b3f686f5d655e3975d85f58ff513e2
Mikhail Bobrutskov <wizzardobox@gmail.com>
2023-12-09 13:35:38 +0100

    optimized string reading

--- common/src/lib.rs
+++ common/src/lib.rs
@@ -2,7 +2,7 @@ use std::str::FromStr;
 
 pub fn get_first_digit_char_index(s: &str, mid: usize) -> usize {
     let mut i = mid;
-    while i > 0 && s.chars().nth(i - 1).unwrap().is_digit(10) {
+    while i > 0 && char_at(s, i - 1).is_digit(10) {
         i = i - 1;
     }
     return i;
@@ -10,23 +10,27 @@ pub fn get_first_digit_char_index(s: &str, mid: usize) -> usize {
 
 pub fn get_last_digit_char_index(s: &str, mid: usize) -> usize {
     let mut i = mid;
-    while i < s.len() && s.chars().nth(i).unwrap().is_digit(10) {
+    while i < s.len() && char_at(s, i).is_digit(10) {
         i = i + 1;
     }
     return i;
+}
+
+fn char_at(s: &str, i: usize) -> char {
+    (&s[i..]).chars().nth(0).unwrap()
 }
 
 pub fn read_numbers<T>(line: &str, mut from: usize, to: usize, into: &mut [T]) -> usize
     where T: FromStr, <T as FromStr>::Err: std::fmt::Debug {
     let mut count = 0;
     while from < to {
-        if line.chars().nth(from).unwrap() <= ' ' {
+        if char_at(line, from) <= ' ' {
             from += 1;
             continue;
         }
 
         let end: usize;
-        if line.chars().nth(from).unwrap() == '-' {
+        if char_at(line, from) == '-' {
             end = get_last_digit_char_index(line, from + 1);
         } else {
             end = get_last_digit_char_index(line, from);
